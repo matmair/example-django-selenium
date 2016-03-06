1cda58eb53444db728fcf876bd855f38d255e5ed
erdem <erdemozkol@gmail.com>
2016-03-06 15:10:12 +0200

    implemented hello view

--- todoapp/accounts/urls.py
+++ todoapp/accounts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     # url(r'^profile/$', "profile", name="profile"),
-    # url(r'^register/$', "register", name="register"),
+    url(r'^register/$', "register", name="register"),
     url(r'^login/$', "login", name="login"),
     # url(r'^logout/$', "logout", name="logout"),
 ]
--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls import  include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
+from hello.views import HelloView
+
 
 urlpatterns = [
-    url(r'^', include("todos.urls", namespace="todos")),
-    url(r'^accounts/', include("accounts.urls", namespace="users")),
+    url(r'^$', HelloView.as_view()),
+    url(r'^todos/', include("todos.urls", namespace="todos")),
+    url(r'^accounts/', include("accounts.urls", namespace="accounts")),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
