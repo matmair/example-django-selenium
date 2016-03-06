d017efa5011b620962096ba7c4bf77812874133b
erdem <erdemozkol@gmail.com>
2016-03-06 12:14:01 +0200

    implemented first dummy selenium test

--- todoapp/accounts/tests.py
+++ todoapp/accounts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class MySeleniumTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(MySeleniumTests, cls).setUpClass()
+        cls.browser = webdriver.Firefox()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def setUp(self):
+        super(MySeleniumTests, self).setUp()
+
+    def test_login(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_xpath('//input[@type="text"]').click()
+        self.browser.find_element_by_xpath('//a').click()
+
