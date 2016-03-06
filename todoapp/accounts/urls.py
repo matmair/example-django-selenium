3503b16be66a17af1cc3a377f70d66ad847d344d
erdem <erdemozkol@gmail.com>
2016-03-06 15:39:35 +0200

    implemented user login and registration views

--- todoapp/accounts/urls.py
+++ todoapp/accounts/urls.py
@@ -1,8 +1,9 @@
+from accounts.views import UserRegistrationView, UserLoginView
 from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^register/$', UserRegistrationView.as_view(), name="register"),
+    url(r'^login/$', UserLoginView.as_view(), name="login"),
     # url(r'^profile/$', "profile", name="profile"),
-    url(r'^register/$', "register", name="register"),
-    url(r'^login/$', "login", name="login"),
     # url(r'^logout/$', "logout", name="logout"),
 ]
--- todoapp/accounts/views.py
+++ todoapp/accounts/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
+from accounts.forms import UserRegistrationForm
 from django.views.generic import FormView
 
 
-class LoginView(FormView):
+class UserLoginView(FormView):
     template_name = "accounts/login.html"
     form_class = AuthenticationForm
-    success_url = ""
+    success_url = "/"
+
+
+class UserRegistrationView(FormView):
+    template_name = "accounts/register.html"
+    form_class = UserRegistrationForm
+    success_url = "/"
