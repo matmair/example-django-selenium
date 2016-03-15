8b305c0bcc562a47a08f7def02ea00980d3ed7be
erdem <erdemozkol@gmail.com>
2016-03-15 23:21:00 +0000

    implemented take_screenshot method for selenium tests

--- todoapp/accounts/tests.py
+++ todoapp/accounts/tests.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import os
 from selenium import webdriver
+
+SCREENSHOT_DUMP_LOCATION = os.path.join(settings.BASE_DIR, 'screendumps')
 
 
 class UserRegistrationSeleniumTestCase(StaticLiveServerTestCase):
@@ -12,8 +17,22 @@ class UserRegistrationSeleniumTestCase(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def take_screenshot(self):
+        filename = self.get_filename()
+        self.browser.get_screenshot_as_file(filename)
+
+    def get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-{timestamp}.png'.format(
+            folder=SCREENSHOT_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            timestamp=timestamp
+        )
+
     def test_user_registration(self):
         self.browser.find_element_by_id("id-register").click()
+        self.take_screenshot()
         self.browser.find_element_by_id("id_username").send_keys("erdem12")
         self.browser.find_element_by_id("id_email").send_keys("erdem12@erdem.com")
         self.browser.find_element_by_id("id_password1").send_keys("cd89c9270")
